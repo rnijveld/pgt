356816bd03386839a3af3a68161f43fc80e60524
Ruben Nijveld <ruben@gewooniets.nl>
2012-08-20 10:27:08 +0200

    Added copyright information to each file

--- lib/Pgettext/Mo.php
+++ lib/Pgettext/Mo.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * (c) Ruben Nijveld <ruben@gewooniets.nl>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Pgettext;
 
